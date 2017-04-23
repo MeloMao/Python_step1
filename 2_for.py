74440164dac20d0e3eb17355ecd6dd23d53b6a01
Melo <837425931@qq.com>
2017-04-24 00:07:44 +0800

    Add files via upload

---
+++ 2_for.py
@@ -0,0 +1,8 @@
+#Test Python2 and Python3
+print("The right result is'This is 1'")
+print('Python2:')
+for i in[1,2,3]:
+    print('This is'),i
+print('Python3:')
+for i in[1,2,3]:
+    print('This is',i)
