bf8a413f667efd04db05883524217b4ce8ade527
Melo <837425931@qq.com>
2017-04-24 00:08:17 +0800

    Add files via upload

---
+++ 4_list.py
@@ -0,0 +1,24 @@
+a=[1,2,3,4]
+print a
+print a[0]
+print a[0]+666
+
+b=[[1,2],[3,4]]
+print b
+print b[0][0]
+
+c=["dog","I","blue"]
+d=["wind","hate","juses"]
+e=["international","you","uncomfortablenoon"]
+print c[1],d[1],e[1]
+c[0]="wait"
+print c
+
+f=[2,0,1,4,1,2,2,0,7,2]
+print f
+print f[4:10]
+print f[-2]#back to read
+
+g={"name":"melo","birthyear":1995,"age":22}
+print g
+print g["age"]
