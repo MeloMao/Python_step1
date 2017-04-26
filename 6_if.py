e7892392f6acccc293109ff949c670dbbeeef725
Melo <837425931@qq.com>
2017-04-26 21:16:11 +0800

    Add files via upload

---
+++ 6_if.py
@@ -0,0 +1,13 @@
+a=6
+if(a>8):
+    print'yes'
+else:
+    print'no'
+
+print a>8 or a>8
+print a>8 or a>3
+print a>3 or a>8
+print a>3 or a>3
+
+print'yes' if a>8 else 'no'
+print'yes' if a>3 else 'no'
