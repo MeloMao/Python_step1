89cd09b1d45917ceaa4a7eba0dcc4835afa50d56
Melo <837425931@qq.com>
2017-07-11 16:07:29 +0800

    The first time to attempt 'try expect'

---
+++ 028_try.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+try:
+    i>8
+except:
+    print 'It has mistake'
+else:
+    print 'It is prefect'
+
+try:
+    i=8
+except:
+    print 'It has mistake'
+else:
+    print 'It is prefect'
